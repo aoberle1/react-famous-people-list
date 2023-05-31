47ffbd4f304daa058b319e774786ebe081417e38
Adam Oberle <oberle.adam@gmail.com>
2023-05-31 12:20:43 -0500

    about to branch

--- src/components/FamousSection/FamousSection.jsx
+++ src/components/FamousSection/FamousSection.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './FamousSection.css';
 import axios from 'axios';
+import FamousPersonList from '../FamousPersonList/FamousPersonList'
 
 function FamousSection() {
   let [famousPersonName, setPersonName] = useState('');
